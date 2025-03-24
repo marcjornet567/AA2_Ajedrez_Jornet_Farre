06ea2c301b374b0ba4f0bb22d39aedbaebc56b76
Marc Jornet <tematoprimo19@gmail.com>
2025-03-24 10:25:06 +0100

    taulell
    bxcd

--- Ajedrez/main.cpp
+++ Ajedrez/main.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
 
+void IniciarTablero(char chessBoard[][]) {
+	//Funcionament Taulell
+	for (int i = 0; i < ; i++)
+	{
+		for (int j = 0; j < ; j++)
+		{
+			chessBoard[i][j] = '*';
+		}
+	}
+
+}
+
+void PrinteoTablero(char chessBoard[][]){
+	cout << '  ';
+
+	for (int i = 0; i < ; i++)
+	{
+		cout << i+1 << ' ';
+	}
+	cout << endl;
+
+	for (int i = 0; i < ; i++)
+	{
+		cout << 8-i << ' ';
+		for (int j = 0; j < ; j++)
+		{
+			cout << chessBoard[i][j] = '*';
+		}
+		cout << endl;
+	}
+}
+
 int main() {
+	//Creacio de Taulell
+	char chessBoard[][];
+	iniciarTablero(chessBoard);
+	PrinteoTablero(chessBoard);
+	
+
 	
 }
