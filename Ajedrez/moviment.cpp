cdea4f60dfe595b8275768b8ef2385c729cee773
polfarr <polf083@gmail.com>
2025-05-03 12:35:32 +0200

    Movimiento

---
+++ Ajedrez/moviment.cpp
@@ -0,0 +1,100 @@
+#include "const.h"
+#include "utils.h"
+
+position MovimientoReina(char chessBoard[TAMANY_TAULELL][TAMANY_TAULELL], position userGetPice, position setPiceByUser) {
+	
+	// Movimiento horizontal
+
+	for (int i = 0; i < TAMANY_TAULELL; i++) {
+		if (chessBoard[userGetPice.x][i] == ESPAI) {
+			chessBoard[userGetPice.x][i] = chessBoard[userGetPice.x][userGetPice.y];
+			chessBoard[userGetPice.x][userGetPice.y] = ESPAI;
+		}
+	}
+
+	// Movimiento vertical
+
+	for (int i = 0; i < TAMANY_TAULELL; i++) {
+		if (chessBoard[i][userGetPice.y] == ESPAI) {
+			chessBoard[i][userGetPice.y] = chessBoard[userGetPice.x][userGetPice.y];
+			chessBoard[userGetPice.x][userGetPice.y] = ESPAI;
+		}
+	}
+	// Movimiento diagonal
+
+	for (int i = 0; i < TAMANY_TAULELL; i++) {
+		if (chessBoard[i][i] == ESPAI) {
+			chessBoard[i][i] = chessBoard[userGetPice.x][userGetPice.y];
+			chessBoard[userGetPice.x][userGetPice.y] = ESPAI;
+		}
+	}
+	return setPiceByUser;
+}
+
+position MovimientoRei(char chessBoard[TAMANY_TAULELL][TAMANY_TAULELL], position userGetPice, position setPiceByUser) {
+
+	// Movimiento horizontal
+	if (userGetPice.x == setPiceByUser.x && userGetPice.y != setPiceByUser.y) {
+		chessBoard[userGetPice.x][setPiceByUser.y] = chessBoard[userGetPice.x][userGetPice.y];
+		chessBoard[userGetPice.x][userGetPice.y] = ESPAI;
+	}
+	// Movimiento vertical
+	else if (userGetPice.x != setPiceByUser.x && userGetPice.y == setPiceByUser.y) {
+		chessBoard[setPiceByUser.x][userGetPice.y] = chessBoard[userGetPice.x][userGetPice.y];
+		chessBoard[userGetPice.x][userGetPice.y] = ESPAI;
+	}
+	// Movimiento diagonal
+	else if (abs(userGetPice.x - setPiceByUser.x) == abs(userGetPice.y - setPiceByUser.y)) {
+		chessBoard[setPiceByUser.x][setPiceByUser.y] = chessBoard[userGetPice.x][userGetPice.y];
+		chessBoard[userGetPice.x][userGetPice.y] = ESPAI;
+	}
+	return setPiceByUser;
+}
+
+position MovimientoCaball(char chessBoard[TAMANY_TAULELL][TAMANY_TAULELL], position userGetPice, position setPiceByUser) {
+	// Movimiento en L
+	if ((abs(userGetPice.x - setPiceByUser.x) == 2 && abs(userGetPice.y - setPiceByUser.y) == 1) || (abs(userGetPice.x - setPiceByUser.x) == 1 && abs(userGetPice.y - setPiceByUser.y) == 2)) {
+		chessBoard[setPiceByUser.x][setPiceByUser.y] = chessBoard[userGetPice.x][userGetPice.y];
+		chessBoard[userGetPice.x][userGetPice.y] = ESPAI;
+	}
+	return setPiceByUser;
+}
+
+position MovimientoAlfil(char chessBoard[TAMANY_TAULELL][TAMANY_TAULELL], position userGetPice, position setPiceByUser) {
+	// Movimiento diagonal
+	if (abs(userGetPice.x - setPiceByUser.x) == abs(userGetPice.y - setPiceByUser.y)) {
+		chessBoard[setPiceByUser.x][setPiceByUser.y] = chessBoard[userGetPice.x][userGetPice.y];
+		chessBoard[userGetPice.x][userGetPice.y] = ESPAI;
+	}
+	return setPiceByUser;
+}
+
+position MovimientoTorre(char chessBoard[TAMANY_TAULELL][TAMANY_TAULELL], position userGetPice, position setPiceByUser) {
+	// Movimiento horizontal
+	if (userGetPice.x == setPiceByUser.x && userGetPice.y != setPiceByUser.y) {
+		chessBoard[userGetPice.x][setPiceByUser.y] = chessBoard[userGetPice.x][userGetPice.y];
+		chessBoard[userGetPice.x][userGetPice.y] = ESPAI;
+	}
+	// Movimiento vertical
+	else if (userGetPice.x != setPiceByUser.x && userGetPice.y == setPiceByUser.y) {
+		chessBoard[setPiceByUser.x][userGetPice.y] = chessBoard[userGetPice.x][userGetPice.y];
+		chessBoard[userGetPice.x][userGetPice.y] = ESPAI;
+	}
+	return setPiceByUser;
+}
+
+position MovimientoPeon(char chessBoard[TAMANY_TAULELL][TAMANY_TAULELL], position userGetPice, position setPiceByUser) {
+	// Movimiento vertical
+	if (userGetPice.x == setPiceByUser.x && userGetPice.y != setPiceByUser.y) {
+		chessBoard[userGetPice.x][setPiceByUser.y] = chessBoard[userGetPice.x][userGetPice.y];
+		chessBoard[userGetPice.x][userGetPice.y] = ESPAI;
+	}
+	// Movimiento diagonal
+	else if (abs(userGetPice.x - setPiceByUser.x) == 1 && abs(userGetPice.y - setPiceByUser.y) == 1) {
+		chessBoard[setPiceByUser.x][setPiceByUser.y] = chessBoard[userGetPice.x][userGetPice.y];
+		chessBoard[userGetPice.x][userGetPice.y] = ESPAI;
+	}
+	return setPiceByUser;
+}
+
+position
--- Ajedrez/taulell.cpp
+++ Ajedrez/taulell.cpp
@@ -68,6 +68,7 @@ void PrinteoTablero(char chessBoard[TAMANY_TAULELL][TAMANY_TAULELL]) {
 	}
 }
 
+// Funcio per agafar una peça del taulell
 position getPiceByUser(char chessBoard[TAMANY_TAULELL][TAMANY_TAULELL], bool whiteTurn) {
 
 	position getPiceByUser;
@@ -82,19 +83,19 @@ position getPiceByUser(char chessBoard[TAMANY_TAULELL][TAMANY_TAULELL], bool whi
 		cin >> getPiceByUser.x;
 		cout << "Y: ";
 		cin >> getPiceByUser.y;
-		
+
 		getPiceByUser.x--;
 		getPiceByUser.y--;
 
 		getPiceByUser.y = TAMANY_TAULELL - getPiceByUser.y - 1;
 
-		if (getPiceByUser.x < 0 || getPiceByUser.x > TAMANY_TAULELL-1 || getPiceByUser.y < 1 || getPiceByUser.y > TAMANY_TAULELL-1) 
+		if (getPiceByUser.x < 0 || getPiceByUser.x > TAMANY_TAULELL - 1 || getPiceByUser.y < 1 || getPiceByUser.y > TAMANY_TAULELL - 1)
 		{
 			cout << "INPUT INVALID!!!! Fora dels limits" << endl;
-			isGettingAPice = false; 
-			
+			isGettingAPice = false;
+
 		}
-		else if(chessBoard[getPiceByUser.x][getPiceByUser.y] == ESPAI)
+		else if (chessBoard[getPiceByUser.x][getPiceByUser.y] == ESPAI)
 		{
 			cout << "INPUT INVALID!!!! No hi ha cap pesa" << endl;
 			isGettingAPice = false;
@@ -108,4 +109,49 @@ position getPiceByUser(char chessBoard[TAMANY_TAULELL][TAMANY_TAULELL], bool whi
 	} while (getPiceByUser.x < 1 || getPiceByUser.x > TAMANY_TAULELL || getPiceByUser.y < 1 || getPiceByUser.y > TAMANY_TAULELL);
 
 	return getPiceByUser;
+}
+
+
+
+// Funcio per posar una peça al taulell
+position PutPiceByUser(char chessBoard[TAMANY_TAULELL][TAMANY_TAULELL], bool whiteTurn) {
+
+	position PutPiceByUser;
+	bool isPuttingAPice = true;
+	do
+	{
+		isPutingAPice = true;
+		cout << "---------------" << endl;
+		cout << "Elige una pieza:" << endl;
+		cout << "---------------" << endl;
+		cout << "X: ";
+		cin >> PutPiceByUser.x;
+		cout << "Y: ";
+		cin >> PutPiceByUser.y;
+
+		PutPiceByUser.x--;
+		PutPiceByUser.y--;
+
+		PutPiceByUser.y = TAMANY_TAULELL - PutPiceByUser.y - 1;
+
+		if (PutPiceByUser.x < 0 || PutPiceByUser.x > TAMANY_TAULELL - 1 || PutPiceByUser.y < 1 || PutPiceByUser.y > TAMANY_TAULELL - 1)
+		{
+			cout << "INPUT INVALID!!!! Fora dels limits" << endl;
+			isPuttingAPice = false;
+
+		}
+		else if (chessBoard[PutPiceByUser.x][PutPiceByUser.y] == ESPAI)
+		{
+			cout << "INPUT INVALID!!!! No hi ha cap pesa" << endl;
+			isPuttingAPice = false;
+		}
+		else if ((whiteTurn && chessBoard[PutPiceByUser.x][PutPiceByUser.y] >= 'a' && chessBoard[PutPiceByUser.x][PutPiceByUser.y] >= 'z') || (whiteTurn && chessBoard[PutPiceByUser.x][getPiceByUser.y] >= 'A' && chessBoard[getPiceByUser.x][getPiceByUser.y] >= 'Z'))
+		{
+			cout << "INPUT INVALID!!!! no pots agafar una pesa del rival" << endl;
+			isPuttingAPice = false;
+		}
+
+	} while (PutPiceByUser.x < 1 || PutPiceByUser.x > TAMANY_TAULELL || PutPiceByUser.y < 1 || PutPiceByUser.y > TAMANY_TAULELL);
+
+	return PuticeByUser;
 }
