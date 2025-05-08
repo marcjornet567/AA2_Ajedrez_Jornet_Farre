c997bf5821080644233b3b794f6e72bfe1fbc7f4
Marc Jornet <tematoprimo19@gmail.com>
2025-05-08 11:59:09 +0200

    commit un tanto inutil

--- Ajedrez/main.cpp
+++ Ajedrez/main.cpp
@@ -4,32 +4,6 @@
 #include "moviment.h"
 #include "Peca.h"
 
-
-//int main() {
-//	//Creacio de Taulell
-//	char chessBoard[TAMANY_TAULELL][TAMANY_TAULELL];
-//	bool gameOver = false;
-//	bool isWhiteTrue = true;
-//	IniciarTablero(chessBoard);
-//
-//	do
-//	{
-//		PrinteoTablero(chessBoard);
-//
-//		position userGetPice, setPiceByUser; //set pice va despres
-//
-//		userGetPice = getPiceByUser(chessBoard, isWhiteTrue);
-//
-//		isWhiteTrue = !isWhiteTrue;
-//
-//		system("cls");
-//	} while (gameOver);
-//
-//	
-//	
-//}
-// Función principal del programa
-
 int main() {
 
     // Declaración del tablero
